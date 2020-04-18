18a5f75f6d285d9f17370b99e6a1e3c5be2945da
Indunil Sanjeewa Amarasinghe <indunil_amarasinghe@hotmail.com>
2020-04-18 16:36:01 +0530

    Committed changes with bootstrap navigation bar with fruit bowl image.

--- FruitsAndVeggiesApp/Site.Master.designer.cs
+++ FruitsAndVeggiesApp/Site.Master.designer.cs
@@ -7,18 +7,11 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace FruitsAndVegApp {
-    
-    
-    public partial class Site {
-        
-        /// <summary>
-        /// contentPlaceHolderID1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder contentPlaceHolderID1;
+namespace FruitsAndVegApp
+{
+
+
+    public partial class Site
+    {
     }
 }
